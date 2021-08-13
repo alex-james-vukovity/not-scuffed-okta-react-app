8c3aa04050cd3606defac53780cc08796ff0f4e1
alex-james-vukovity <alex.vukovity@gmail.com>
2021-08-13 20:38:01 +0100

    config: okta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, useHistory } from "react-router-dom"
+import { Security, LoginCallback, useOktaAuth, SecureRoute } from "@okta/okta-react"
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js"
 
-function App() {
+const Home = () => {
+  const { oktaAuth } = useOktaAuth()
+
+  const logout = async () => oktaAuth.signOut()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Logged in</h1>
+      <button onClick={logout}>Log out</button>
     </div>
-  );
+  )
 }
 
-export default App;
+const Okta = ({ children }) => {
+  const config = {
+    clientId: "0oa1go71zlYEElmQ65d7",
+    issuer: "https://dev-28193283.okta.com/oauth2/default",
+    redirectUri: "http://localhost:3000/web/login/callback",
+    scopes: ["openid", "profile", "email"],
+    postLogoutRedirectUri: `${window.location.origin}/web`,
+  }
+
+  const oktaAuth = new OktaAuth(config)
+  const { replace } = useHistory()
+  const restoreOriginalUri = async (_oktaAuth) =>
+    replace(toRelativeUrl("/web", window.location.origin))
+
+  return (
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      {children}
+    </Security>
+  )
+}
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Okta>
+        <Route path="/web/login/callback" component={LoginCallback} />
+
+        <SecureRoute path="/web" exact>
+          <Home />
+        </SecureRoute>
+
+        <SecureRoute path="/web/secure" exact>
+          <h1>This is a secure route</h1>
+        </SecureRoute>
+      </Okta>
+    </BrowserRouter>
+  )
+}
